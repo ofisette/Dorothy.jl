89fbd683791e01d78e8913c5ce86b10e4ea99f15
Olivier Fisette <ofisette@gmail.com>
2019-01-08 16:18:13 +0100

    Use FormatCodecs to read compressed test data.

--- tests/selection.jl
+++ tests/selection.jl
@@ -3,6 +3,7 @@ using Dorothy
 using .Selectors
 using Formats
 using FormatStreams
+using FormatCodecs
 
 datapath = joinpath(@__DIR__, "..", "data")
 
@@ -16,7 +17,9 @@ datapath = joinpath(@__DIR__, "..", "data")
 		@test length(view(model, Water)) == 199
 		@test length(view(model, Water | Protein)) == 2032 + 199
 		@test length(view(model, Water & Protein)) == 0
+		@test length(view(model, Index(1, by=Residue))) == 10
 		@test length(view(model, Expand(Index(1), by=Residue))) == 10
+		@test length(view(model, Index(1, by=Chain))) == 2236
 		@test length(view(model, Expand(Index(1), by=Chain))) == 2236
 		sel2 = view(model, Water & Within(5.0, of=Nter))
 		@test length(sel2) == 7
@@ -36,7 +39,7 @@ datapath = joinpath(@__DIR__, "..", "data")
 	end
 
 	@testset "PLC" begin # Very large orthorhombic system
-		model = readf("$(datapath)/PLC.pdb")
+		model = readf("$(datapath)/PLC.pdb.bz2")
 		sel1 = view(model, Water & Expand(Within(3.0, of=Lipid), by=Residue))
 	end
 
