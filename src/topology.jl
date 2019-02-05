f7e1a1107d728d468d4ee6ddfaf1a2359430acff
Olivier Fisette <ofisette@gmail.com>
2019-02-05 12:54:57 +0100

    Optimised topology detection from covalent radii

--- src/topology.jl
+++ src/topology.jl
@@ -6,11 +6,20 @@ using ..Dorothy.Geometry
 using ..Dorothy.Graphs
 using ..Dorothy.PBC
 using ..Dorothy.Properties
+using ..Dorothy.Selectors
 using ..Dorothy.Utils
 
-export TopologyInferenceStrategy, infertopology!, TopologyFromCovalentRadii
+export
+		TopologyInferenceStrategy, infertopology, infertopology!,
+		TopologyFromCovalentRadii
 
 abstract type TopologyInferenceStrategy end
+
+infertopology(model::ParticleCollection) =
+		infertopology!(Graph(length(model)), model)
+
+infertopology(model::ParticleCollection, strategy::TopologyInferenceStrategy) =
+		infertopology!(Graph(length(model)), model, strategy)
 
 infertopology!(model::ParticleCollection) =
 		infertopology!(get!(model, :topology, []), model)
@@ -21,15 +30,22 @@ infertopology!(model::ParticleCollection, strategy::TopologyInferenceStrategy) =
 struct TopologyFromCovalentRadii <: TopologyInferenceStrategy
 	radii::Dict{String,Float64}
 	rdef::Float64
-	tol::Float64
-	dmax::Float64
+	rtol::Float64
+	dmaxXX::Float64
+	dmaxXH::Float64
+	dmaxHH::Float64
 
 	function TopologyFromCovalentRadii(;
 			radii::AbstractDict{<:AbstractString,<:Real} = covalent_radii,
 			rdef::Real = radii["C"], tol::Real = 0.2)
 		@boundscheck tol >= 0.0 || error("expected positive tolerance")
-		dmax = (1.0+tol) * 2.0 * maximum(values(radii))
-		new(radii, rdef, tol, dmax)
+		rtol = 1.0 + tol
+		rmax = maximum(values(radii))
+		rH = radii["H"]
+		dmaxXX = rtol * (rmax + rmax)
+		dmaxXH = rtol * (rmax + rH)
+		dmaxHH = rtol * (rH + rH)
+		new(radii, rdef, rtol, dmaxXX, dmaxXH, dmaxHH)
 	end
 end
 
@@ -38,36 +54,48 @@ infertopology!(topology::AbstractGraph, model::ParticleCollection) =
 
 function infertopology!(topology::AbstractGraph, model::ParticleCollection,
 		strategy::TopologyFromCovalentRadii)
-	@boundscheck length(topology) == length(model) ||
+	n = length(topology)
+	@boundscheck length(model) == n ||
 			error("size mismatch between model and output array")
 	elements = infermissingelements(model)
+	radii = [get(strategy.radii, elements[i], strategy.rdef) for i = 1:n]
+	H = map(Hydrogen, model)
+	X = .!H
 	cell, Rw, Kw = pbcstrategy(model)
-	lattice = proxilattice(Kw, cell, strategy.dmax)
-	topcov!(topology, elements, Rw, Kw, cell, lattice, strategy.radii,
-			strategy.rdef, strategy.tol)
+	XXlattice = proxilattice(Kw, cell, strategy.dmaxXX)
+	XHlattice = proxilattice(Kw, cell, strategy.dmaxXH)
+	HHlattice = proxilattice(Kw, cell, strategy.dmaxHH)
+	topcov!(topology, radii, Rw, Kw, cell, XXlattice, X, X, strategy.rtol)
+	topcov!(topology, radii, Rw, Kw, cell, XHlattice, X, H, strategy.rtol)
+	topcov!(topology, radii, Rw, Kw, cell, HHlattice, H, H, strategy.rtol)
+	topology
 end
 
-function topcov!(topology::AbstractGraph,
-		elements::AbstractVector{<:AbstractString},
+function topcov!(topology::AbstractGraph, radii::AbstractVector{<:Real},
 		Rw::AbstractVector{Vector3D}, Kw::AbstractVector{Vector3D},
 		cell::Union{TriclinicPBC,Nothing}, lattice::ProximityLattice,
-		radii::AbstractDict{<:AbstractString,<:Real}, rdef::Real, tol::Real)
-	@boundscheck length(topology) == length(elements) == length(Rw) ==
-			length(Kw) || error("size mismatch between property arrays")
-	tolf = 1.0 + tol
+		Itargets::AbstractVector{Bool}, Jtargets::AbstractVector{Bool},
+		rtol::Real)
+	n = length(topology)
+	@boundscheck begin
+		length(radii) == length(Rw) == length(Kw) == length(lattice) ==
+				length(Itargets) == length(Jtargets) == n ||
+				error("size mismatch between property arrays")
+		rtol >= 1.0 || error("expected >= 1.0 relative tolerance")
+	end
 	J = Int[]
-	for i in eachindex(Rw)
-		for j in findnear!(J, lattice, i)
-			if i != j && !((i, j) in topology)
-				ri = get(radii, elements[i], rdef)
-				rj = get(radii, elements[j], rdef)
-				if mindist(Rw[i], Kw[i], Rw[j], Kw[j], cell) < tolf * (ri+rj)
-					pair!(topology, (i, j))
+	for i = 1:n
+		if Itargets[i]
+			for j in findnear!(J, lattice, i)
+				if Jtargets[j] && (i != j)
+					if mindist(Rw[i], Kw[i], Rw[j], Kw[j], cell) <
+							rtol * (radii[i] + radii[j])
+						pair!(topology, (i, j))
+					end
 				end
 			end
 		end
 	end
-	topology
 end
 
 end
